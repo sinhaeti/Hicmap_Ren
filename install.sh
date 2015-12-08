c56cf3b6f29a9db85bf93e6f6cc5076e0c510b2b
Rongxin@silencer <r3fang@ucsd.edu>
2015-12-07 17:19:13 -0800

    update pair2mates.c

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 gcc -I htslib -L htslib src/pair2mates.c src/utils.c htslib/libhts.a -lz -lpthread -o bin/pair2mates
 chmod u+x bin/hicmap
 tar -xzf data/mm9.MboI.500bp.tar.gz -C data/
+tar -xzf data/hg19.Hind3.500bp.tar.gz -C data/
+tar -xzf data/mm9.Hind3.500bp.tar.gz -C data/

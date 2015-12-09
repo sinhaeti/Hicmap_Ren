3d76ecd98b22f18bef6149602b05b2d365187ddd
Rongxin@silencer <r3fang@ucsd.edu>
2015-12-09 14:44:57 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 gcc -I htslib -L htslib src/pair2mates.c src/utils.c htslib/libhts.a -lz -lpthread -o bin/pair2mates
 chmod +x bin/hicmap
 chmod +x bin/chimeric.pl
+chmod +x bin/chipseq
 tar -xzf data/mm9.MboI.500bp.tar.gz -C data/
 tar -xzf data/hg19.Hind3.500bp.tar.gz -C data/
 tar -xzf data/mm9.Hind3.500bp.tar.gz -C data/

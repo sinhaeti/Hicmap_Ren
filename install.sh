7fca0c9d4475ac641223f17e633da8edc66673b3
r3fang <r3fang@ucsd.edu>
2016-06-28 09:40:59 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
+cd htslib/
+./configure    # Optional, needed for choosing optional functionality
+make
+cd ../
 gcc -I htslib -L htslib src/pair2mates.c src/utils.c htslib/libhts.a -lz -lpthread -o bin/pair2mates
 chmod +x bin/hicmap
 chmod +x bin/chimeric.pl
 chmod +x bin/chipseq
-tar -xzf data/mm9.MboI.500bp.tar.gz -C data/
-tar -xzf data/hg19.MboI.500bp.tar.gz -C data/
-tar -xzf data/hg19.Hind3.500bp.tar.gz -C data/
-tar -xzf data/mm9.Hind3.500bp.tar.gz -C data/

6957b6e3f19919a7566fef6e5f74785d71af888a
r3fang <r3fang@ucsd.edu>
2016-07-01 15:42:26 -0700

    rm data/

--- install.sh
+++ install.sh
@@ -12,5 +12,4 @@ chmod +x bin/atacseq
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "export PATH=\$PATH:$DIR/bin" >> ~/.bash_profile
-echo "export R_LIBS=$R_LIBS" >> ~/.bash_profile
 bash ~/.bash_profile

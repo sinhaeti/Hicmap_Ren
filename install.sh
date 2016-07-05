4f987aa061fb5263af1e6940a6ad743fd42c5bd0
r3fang <r3fang@ucsd.edu>
2016-07-04 21:37:51 -0700

    update hicmap

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ chmod +x bin/hicmap
 chmod +x bin/chimeric.pl
 chmod +x bin/chipseq
 chmod +x bin/atacseq
+chmod +x bin/hicmap_cutter_sites_filter
+chmod +x bin/hicmap_pair_up_filter
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "export PATH=\$PATH:$DIR/bin" >> ~/.bash_profile
